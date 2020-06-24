052d2cef21d6abf32817650ef61cdf39cab1c856
Brett Morris <brettmorris21@gmail.com>
2020-06-24 10:02:17 +0200

    adding docstrings to pyx

--- kelp/fast.pyx
+++ kelp/fast.pyx
@@ -5,9 +5,8 @@ from cython.parallel import prange
 
 from libc.math cimport sin, cos, exp, pi
 
-__all__ = ["h_ml_sum_cy", "blackbody",# "trapz3d", #"blackbody2d",
-           #"bilinear_interpolate",
-           "integrate_planck", "integrated_blackbody"]
+__all__ = ["h_ml_sum_cy", "blackbody", "integrate_planck",
+           "integrated_blackbody"]
 
 DTYPE = np.float64
 
@@ -166,6 +165,9 @@ def h_ml_sum_cy(float hotspot_offset, float omega_drag, float alpha,
 @cython.wraparound(False)
 @cython.cdivision(True)
 cdef float blackbody_lambda(float lam, float temperature) nogil:
+    """
+    Compute the blackbody flux as a function of wavelength `lam` in mks units
+    """
     cdef float h = 6.62607015e-34  # J s
     cdef float c = 299792458.0  # m/s
     cdef float k_B = 1.380649e-23  # J/K
@@ -271,7 +273,9 @@ cdef np.ndarray trapz3d(double [:, :, :] y_3d, double [:] x):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cdef int argmin_lowest(double [:] arr, float value):
-
+    """
+    Return the index of `arr` which is closest to *and less than* `value`
+    """
     cdef int i, min_ind = 0, n = len(arr)
     cdef float dist, min_dist = 1e10
 
@@ -286,7 +290,9 @@ cdef int argmin_lowest(double [:] arr, float value):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cdef int argmin(double [:] arr, float value):
-
+    """
+    Return the index of `arr` which is closest to `value`
+    """
     cdef int i, min_ind = 0, n = len(arr)
     cdef float dist, min_dist = 1e10
 
@@ -305,6 +311,9 @@ cdef float bilinear_interpolate(double [:, :] im,
                                 double [:] x_grid, double [:] y_grid,
                                 float y, float x):
     """
+    Bilinear interpolation over an image `im` which is computed on grid
+    `x_grid` vs `y_grid`, evaluated at position (x, y).
+
     Source: https://stackoverflow.com/a/12729229/1340208
     """
     cdef int minind0, minind1
@@ -337,8 +346,13 @@ def integrate_planck(double [:] filt_wavelength, double [:] filt_trans,
                      double [:, :] temperature, double [:, :] T_s,
                      double [:] theta_grid, double [:] phi_grid, float rp_rs,
                      int n_phi, bint return_interp=True):
-    # cdef Py_ssize_t i, j, l = len(theta_grid), m = len(phi_grid)
-
+    """
+    Integrate the Planck function over wavelength for the temperature map of the
+    planet `temperature` and the temperature of the host star `T_s`. If
+    `return_interp`, returns the interpolation function for the integral over
+    the ratio of the blackbodies over wavelength; else returns only the map
+    (which can be used for trapezoidal approximation integration)
+    """
     bb_num = blackbody2d(filt_wavelength, temperature)
     bb_den = blackbody2d(filt_wavelength, T_s)
     cdef int i, j, k
@@ -361,7 +375,7 @@ def integrate_planck(double [:] filt_wavelength, double [:] filt_trans,
         def interp(theta, phi, theta_grid=theta_grid, phi_grid=phi_grid,
                    rp_rs=rp_rs, int_bb=int_bb):
             return bilinear_interpolate(int_bb, phi_grid, theta_grid,
-                                        theta, phi) #* rp_rs ** 2
+                                        theta, phi)
         return int_bb, interp
     else:
         return int_bb
@@ -373,6 +387,11 @@ def integrated_blackbody(float hotspot_offset, float omega_drag,
                          int lmax, float T_s, float a_rs, float rp_a, float A_B,
                          int n_theta, int n_phi, double [:] filt_wavelength,
                          double [:] filt_transmittance, float f=2**-0.5):
+    """
+    Compute the temperature field using `h_ml_sum_cy`, then integrate the
+    temperature map over wavelength and take the ratio of blackbodies with
+    `integrate_planck`
+    """
     cdef float T_eq, rp_rs
     cdef np.ndarray phi = np.linspace(-2 * pi, 2 * pi, n_phi, dtype=DTYPE)
     cdef np.ndarray theta = np.linspace(0, pi, n_theta, dtype=DTYPE)
@@ -403,6 +422,9 @@ def phase_curve(double [:] xi, float hotspot_offset, float omega_drag,
                 int lmax, float T_s, float a_rs, float rp_a, float A_B,
                 int n_theta, int n_phi, double [:] filt_wavelength,
                 double [:] filt_transmittance, float f=2**-0.5):
+    """
+    Compute the phase curve evaluated at phases `xi`.
+    """
     cdef float T_eq, rp_rs
     cdef int k, phi_min, phi_max, n_xi = len(xi)
     cdef np.ndarray phi = np.linspace(-2 * pi, 2 * pi, n_phi, dtype=DTYPE)
@@ -437,12 +459,15 @@ def phase_curve(double [:] xi, float hotspot_offset, float omega_drag,
                          sinsq_2d(theta2d[phi_min:phi_max]) *
                          cos_2d(phi2d[phi_min:phi_max] + xi[k]))
         integral = trapz2d(integrand_map, phi[phi_min:phi_max], theta)
-        fluxes[k] = integral #* rp_rs**2
+        fluxes[k] = integral
     return fluxes
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cdef float sum2d(double [:, :] z):
+    """
+    Sum a 2d array over its axes
+    """
     cdef int m = z.shape[0], n = z.shape[1]
     cdef float s = 0
 
@@ -454,6 +479,9 @@ cdef float sum2d(double [:, :] z):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cdef float sum1d(double [:] z):
+    """
+    Sum a 1d array over its first axis
+    """
     cdef int m = z.shape[0]
     cdef float s = 0
 
@@ -464,6 +492,9 @@ cdef float sum1d(double [:] z):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cdef np.ndarray sinsq_2d(double [:, :] z):
+    """
+    The square of the sine of a 2d array
+    """
     cdef int m = z.shape[0], n = z.shape[1]
     cdef np.ndarray s = np.zeros((m, n), dtype=DTYPE)
     cdef double [:, ::1] s_view = s
@@ -477,6 +508,9 @@ cdef np.ndarray sinsq_2d(double [:, :] z):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cdef np.ndarray cos_2d(double [:, :] z):
+    """
+    The cosine of a 2d array
+    """
     cdef int m = z.shape[0], n = z.shape[1]
     cdef np.ndarray s = np.zeros((m, n), dtype=DTYPE)
     cdef double [:, ::1] s_view = s
@@ -490,19 +524,25 @@ cdef np.ndarray cos_2d(double [:, :] z):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cdef float trapz2d(double [:, :] z, double [:] x, double [:] y):
-    # # Source: https://www.mathworks.com/matlabcentral/fileexchange/40631-2d-trapezoidal-rule
-    #
-    # return np.trapz(np.trapz(z, y, axis=1), x)
     """
     Integrates a regularly spaced 2D grid using the composite trapezium rule.
-    IN:
-       z : 2D array
-       x : (optional) grid values for x (1D array)
-       y : (optional) grid values for y (1D array)
-       dx: if x is not supplied, set it to the x grid interval
-       dy: if y is not supplied, set it to the x grid interval
+    
     Source: https://github.com/tiagopereira/python_tips/blob/master/code/trapz2d.py
-    """
+    
+    Parameters
+    ----------
+    z : `~numpy.ndarray`
+        2D array
+    x : `~numpy.ndarray`
+        grid values for x (1D array)
+    y : `~numpy.ndarray`
+        grid values for y (1D array)
+    
+    Returns
+    -------
+    t : `~numpy.ndarray`
+        Trapezoidal approximation to the integral under z
+        """
     cdef float dx, dy, s1, s2, s3
     cdef int m = z.shape[0] - 1, n = z.shape[1] - 1
     dx = x[1] - x[0]
