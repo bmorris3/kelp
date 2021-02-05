5d203724a1c77c00c09e7229189690b1152d937a
Brett Morris <brettmorris21@gmail.com>
2021-02-05 16:21:03 +0100

    Adding kelp logo

--- docs/conf.py
+++ docs/conf.py
@@ -103,6 +103,8 @@ release = package.__version__
 # a list of builtin themes. To override the custom theme, set this to the
 # name of a builtin theme or the name of a custom theme in html_theme_path.
 #html_theme = None
+html_static_path = ['_static']
+html_style = 'kelp.css'
 
 
 html_theme_options = {
@@ -122,7 +124,7 @@ html_theme_options = {
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = ''
+html_favicon = '_static/kelp.ico'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
