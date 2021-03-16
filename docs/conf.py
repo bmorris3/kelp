4b2c8c45c21d34bbf4b6fcfb0011a761c9dc9ac6
Brett Morris <brettmorris21@gmail.com>
2021-03-16 14:23:20 +0100

    git-lfs workaround

--- docs/conf.py
+++ docs/conf.py
@@ -195,7 +195,12 @@ github_issues_url = 'https://github.com/{0}/issues/'.format(setup_cfg['github_pr
 #     target = target.strip()
 #     nitpick_ignore.append((dtype, six.u(target)))
 
-on_rtd = True #os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
-    import git_lfs
-    git_lfs.fetch(setup_cfg['github_project'])
+    # Workaround to install and execute git-lfs on Read the Docs
+    if not os.path.exists('./git-lfs'):
+        os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.1/git-lfs-linux-amd64-v2.7.1.tar.gz')
+        os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz')
+        os.system('./git-lfs install')  # make lfs available in current repository
+        os.system('./git-lfs fetch')  # download content from remote
+        os.system('./git-lfs checkout')  # make local files to have the real content on them
