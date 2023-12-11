923ab1e4b030f54a8948cb6dbb1261b1d1feecad
Brett M. Morris <bmmorris@stsci.edu>
2023-12-11 10:25:32 -0500

    Remove astropy init helper (#41)
    * remove astropy init helper
    
    * CI fix
    
    * version fix

--- kelp/__init__.py
+++ kelp/__init__.py
@@ -1,10 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *   # noqa
-# ----------------------------------------------------------------------------
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
 
 from .core import *  # noqa
 from .registries import *  # noqa
