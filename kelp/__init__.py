bb24089c31aca49c28ba2e1cabcfd3ed3b8caea2
Brett M. Morris <bmmorris@stsci.edu>
2023-12-11 10:21:10 -0500

    version fix

--- kelp/__init__.py
+++ kelp/__init__.py
@@ -1,4 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
 
 from .core import *  # noqa
 from .registries import *  # noqa
